97c2b1134f9f50c74124d5f19b5e9a26f4e42a55
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-01-25 21:48:18 -0500

    Genaración de archivo vagrantfile, instación de docker y docker compose

---
+++ VM/InstalacionDockerCompose.sh
@@ -0,0 +1,12 @@
+#Instalar DOCKER COMPOSE
+#Link de instalación: https://docs.docker.com/compose/install/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+#Verificar la versión
+docker-compose --version
+
+#Si la instalación falla anteriorment, ejecutar la siguiente línea
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
